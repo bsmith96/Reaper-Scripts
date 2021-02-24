8f9e3cf9b1b0c673a5a5f2108ae2a9b46aa1417f
Ben Smith <bs14829@my.bristol.ac.uk>
2021-02-24 18:18:29 +0000

    Updated adaptability of Arm Tracks For Recording
    Added functions, made the script easy to adapt to any project where you regularly arm and disarm many tracks

--- 01 Recording/Arm Tracks For Recording.lua
+++ 01 Recording/Arm Tracks For Recording.lua
@@ -1,10 +1,28 @@
--- Arm tracks for recording ------------------
+-- Arm tracks for recording ---------------
+-- Author: Ben Smith ----------------------
 
 numberOfTracks = reaper.CountTracks(0)
 
--- Record arm all tracks except track 1 (e.g. a folder)
-for i = numberOfTracks, 2, -1
-do
-	track = reaper.GetTrack(0,i-1)
+
+-- Toggle record arm on a specific track
+
+function armTrack(track)
+	track = reaper.GetTrack(0,track-1)
 	reaper.CSurf_OnRecArmChange(track, -1)
 end
+
+
+-- Toggle record arm on a range of tracks
+
+function armRange(firstTrack, lastTrack)
+	for i = lastTrack, firstTrack, -1
+	do  
+		track = reaper.GetTrack(0,i-1)
+		reaper.CSurf_OnRecArmChange(track, -1)
+	end
+end
+
+
+-- Run Script -----------------------------
+-- use the armTrack or armRange functions here to define which tracks you want to record onto. Use the track numbers as they appear on Reaper.
+
