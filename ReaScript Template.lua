0745234c793ab43b7c12946ab4d71b937ce60288
Ben Smith <ben@bensmithsound.uk>
2021-08-09 00:00:13 +0100

    Added a template

---
+++ ReaScript Template.lua
@@ -0,0 +1,33 @@
+--[[
+  @description
+  @author Ben Smith
+  @link
+    Author     https://www.bensmithsound.uk
+    Repository https://github.com/bsmith96/Reaper-Scripts
+  @version 1.0
+  @changelog
+    + Initial version
+  @about
+    # Package name
+    Written by Ben Smith - Month Year
+
+    ### Info
+    *
+
+    ### Usage
+    * 
+
+    ### User customisation
+    *
+]]
+
+
+-- User customisation area ----------------
+
+--------- End of user customisation area --
+
+
+---- Functions
+
+
+-- Run script -----------------------------
