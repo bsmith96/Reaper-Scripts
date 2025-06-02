50cb9d46df95b752b89475a40989ffcfdb3faa64
Ben Smith <ben@bensmithsound.uk>
2025-06-02 15:49:46 +0100

    Made work with 'listen' rather than 'update'

--- Markers/Create marker with name via OSC.lua
+++ Markers/Create marker with name via OSC.lua
@@ -1,5 +1,5 @@
 --[[
-  @description
+  @description Create marker with name via OSC
   @author Ben Smith
   @link
     Author     https://www.bensmithsound.uk
@@ -10,15 +10,38 @@
   @metapackage
   @provides
     [main] . > bsmith96_{filename}.lua
+    osc.lua
+    Qlab5.ReaperOSC
   @about
-    # {Package name}
-    Written by Ben Smith - {Month Year}
+    # Create marker with name via OSC
+    Written by Ben Smith - June 2025
 
     ### Info
-    *
+    * REQUIRES "osc.lua" IN THE SAME PATH
+    * Intended for integration between Reaper and QLab, allowing markers when cues are fired when Multitrack Recording a live show
 
     ### Usage
-    * 
+    * Create a custom OSC shortcut for this script (in the actions menu)
+    * Send this OSC trigger with a string as the first argument. This string will become the name of the marker.
+    * For QLab integration: 
+      * Reaper Preferences > Control/OSC/Web
+      * Add a new device
+        * Device name: QLab
+        * Configure device IP+local port
+        * Device Port: 53000
+        * Device IP: QLAB IP ADDRESS
+        * Local listen port: 53001
+        * Local IP: current IP address
+        * Allow binding messages to REAPER actions and FX learn: TRUE
+        * Pattern config: open config directory
+          * Copy the provided file "Qlab5.ReaperOSC" into this directory
+        * Pattern config: refresh list, then choose Qlab5
+        * Check your QLab settings - you need to ensure Network > OSC Access > No passcode has "view" access
+      * Now, Reaper should send a message to QLab whenever you start reaper playback, "/listen/go/name", which asks Reaper to send the cue name whenever "go" is fired.
+      * In the actions menu, find this script.
+        * Press play on reaper
+        * Press "go" on any cue in Qlab
+        * This should set the custom shortcut for the action to "/qlab/event/workspace/go/name"
 
     ### User customisation
     *
@@ -43,15 +66,24 @@ local scriptName = ({r.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
 local scriptDirectory = ({r.get_action_context()})[2]:sub(1, ({r.get_action_context()})[2]:find("\\[^\\]*$"))
 local scriptFolder = scriptDirectory:gsub(scriptName..".lua", "")
 
+
 package.path = scriptFolder .. '?.lua'
 
 -- Require the osc module
 local osc = require('osc')
 
+-- debug
+--local msg = osc.get()
+--if msg then
+--    reaper.ShowMessageBox("OSC address: " .. msg.address .. ", argument: " .. (msg.arg and msg.arg or "(nil)"), "Info", 0)
+--else
+--    reaper.ShowMessageBox("Invalid or no OSC message", "Error", 0)
+--end
+
+
 -- =========================================================
 -- ======================  FUNCTIONS  ======================
 -- =========================================================
-
 
 -- =========================================================
 -- ======================  UTILITIES  ======================
@@ -78,10 +110,10 @@ r.Undo_BeginBlock()
 -- get OSC message
 local msg = osc.get()
 
+-- Create a marker at the current position, with the args of OSC message as the marker name
+r.AddProjectMarker(0,0,r.GetPlayPosition(),0,msg.arg,-1)
 
-local newName = msg.arg
+-- Send OSC message to QLab to request the cue name from cue ID
 
-
-dbg("didn't work")
 
 r.Undo_EndBlock(scriptName, -1)
